6a93e69f3369088f421cbfec859cd645b4159cf4
nishikawwwa <snrg.164.ring@gmail.com>
2019-11-09 06:56:18 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,9 +46,11 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         $task = new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
@@ -95,9 +97,11 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:10', 
             'content' => 'required|max:191',
         ]);
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -10,11 +10,15 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
